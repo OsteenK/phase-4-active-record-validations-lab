46c2aaa4653382c6fcd763d6f53d86641667189a
osteen <osteen.mutinda@student.moringaschool.com>
2023-03-18 00:36:42 +0300

    Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait_title
+  
+    private
+  
+    def clickbait_title
+      clickbait_words = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+      if clickbait_words.none? { |word| word.match(title) }
+        errors.add(:title, "must be clickbait-y")
+      end
+    end
+
 end
